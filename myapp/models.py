61e9ffdbe587e2f136069ea6152207858cb00800
Zuza <zuzana.strachova@gmail.com>
2016-04-23 17:16:55 +0200

    Edit Comments commit

--- myapp/models.py
+++ myapp/models.py
@@ -9,6 +9,9 @@ class Post(models.Model):
             default=timezone.now)
     published_date = models.DateTimeField(
             blank=True, null=True)
+    def approved_comments(self):
+        return self.comments.filter(approved_comment=True)
+
 
     def publish(self):
         self.published_date = timezone.now()
